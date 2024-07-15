4845a14080e97aadcb4b62d4d3d42c8c2f8a97d4
flutter_dart@kakao.com <flutter_dart@kakao.com>
2024-07-15 22:18:08 +0900

    Feat: Dart 프로젝트 생성

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# template-repo
-템플릿으로 사용될 레포입니다
-
-- Label
-- Pull Request 템플릿
-- Issue 템플릿
-- Release 템플릿
+함수형 프로그래밍 스터디
---
+++ bin/fp_dart.dart
@@ -0,0 +1,5 @@
+import 'package:fp_dart/fp_dart.dart' as fp_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fp_dart.calculate()}!');
+}
